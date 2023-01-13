d483dd917bf38aae0d38603e6ef7f62cecb94185
hamza umar <humar6078@gmail.com>
2023-01-13 11:48:29 +0000

    lecturer module done

--- pages/staff/modules/index.js
+++ pages/staff/modules/index.js
@@ -51,54 +51,47 @@ const StaffModules = () => {
             <HeadTitle title="Lecturer Modules" />
 
             {/* Main Content  */}
-            <div className="relative rounded-t-xl sm:rounded-b-xl bg-white  pb-20 sm:min-h-0">
-                <div className="flex justify-between items-center py-6 px-4 pt-1 sm:pt-4">
-                    <h2 className="text-black-text font-extrabold text-2xl">
-                        My Modules
-                    </h2>
-                </div>
-                <div className="relative px-4 pb-6">
-                    <AnimatePresence mode="wait">
-                        <motion.div
-                            key={currentDate}
-                            initial={{ y: 10, opacity: 0 }}
-                            animate={{ y: 0, opacity: 1 }}
-                            exit={{ y: -10, opacity: 0 }}
-                            transition={{ duration: 0.2 }}>
-                            {lecturerModules?.length > 0 ? (
-                                <div className="space-y-4 sm:space-y-0 sm:grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                                    {lecturerModules?.map((module, index) => {
-                                        return (
-                                            <ModuleCardLecturer
-                                                key={index}
-                                                module={module}
-                                            />
-                                        );
-                                    })}
-                                </div>
-                            ) : (
-                                <ElementNotFound>
-                                    <h2 className="text-xl sm:text-2xl text-primary font-bold">
-                                        No Active Module Availble
-                                    </h2>
-                                    <p className="text-gray-text font-[500]">
-                                        Sorry! You don't have any module checked
-                                        in for the day yet.
-                                    </p>
-                                </ElementNotFound>
-                            )}
-                        </motion.div>
-                    </AnimatePresence>
-                    <div className="fixed bottom-20 right-4">
-                        <button
-                            onClick={() => {
-                                setModalOpen(true);
-                                setModalType("slideUp");
-                            }}
-                            className="inline-flex items-center px-6 py-3 bg-primary text-white border border-transparent rounded-full font-semibold text-xs capitalize tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring ring-blue-300 disabled:opacity:25 transition ease-in-out duration-150 space-x-2">
-                            <CalendarDaysIcon className="h-5 w-5" />
-                            <span className="text-xs ">Check In</span>
-                        </button>
+            <div className="relative space-y-8 sm:space-y-0">
+                <div className="relative rounded-t-xl sm:rounded-b-xl bg-white  pb-20 sm:mt-10 sm:pb-5  sm:min-h-0">
+                    <div className="flex justify-between items-center py-6 px-4 pt-1 sm:pt-4 sm:border-b">
+                        <h2 className="text-black-text font-extrabold text-2xl">
+                            My Modules
+                        </h2>
+                    </div>
+                    <div className="relative px-4 pb-6 sm:pt-6">
+                        <AnimatePresence mode="wait">
+                            <motion.div
+                                key={currentDate}
+                                initial={{ y: 10, opacity: 0 }}
+                                animate={{ y: 0, opacity: 1 }}
+                                exit={{ y: -10, opacity: 0 }}
+                                transition={{ duration: 0.2 }}>
+                                {lecturerModules?.length > 0 ? (
+                                    <div className="space-y-4 sm:space-y-0 sm:grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                                        {lecturerModules?.map(
+                                            (module, index) => {
+                                                return (
+                                                    <ModuleCardLecturer
+                                                        key={index}
+                                                        module={module}
+                                                    />
+                                                );
+                                            },
+                                        )}
+                                    </div>
+                                ) : (
+                                    <ElementNotFound>
+                                        <h2 className="text-xl sm:text-2xl text-primary font-bold">
+                                            No Active Module Availble
+                                        </h2>
+                                        <p className="text-gray-text font-[500]">
+                                            Sorry! You don't have any module
+                                            checked in for the day yet.
+                                        </p>
+                                    </ElementNotFound>
+                                )}
+                            </motion.div>
+                        </AnimatePresence>
                     </div>
                 </div>
             </div>
